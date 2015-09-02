96e65a912eb5f8e14e9cff1d2382eb7ab1cca8b4
carlosgomezr <carlos001909@gmail.com>
2015-09-01 22:13:55 -0600

    Sexta version
    Menu Admin 65% visual

---
+++ cliente/build/generated/src/org/apache/jsp/admin_jsp.java
@@ -0,0 +1,97 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class admin_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Administrador</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body  background=\"BACK2.jpg\">\n");
+      out.write("    <center>\n");
+      out.write("        <br></br>\n");
+      out.write("        <br></br>\n");
+      out.write("        <br></br>\n");
+      out.write("        <h1>Administrador</h1>\n");
+      out.write("        <br></br>\n");
+      out.write("        <br></br>\n");
+      out.write("        <font color=\"white\">\n");
+      out.write("        <table border=\"0\" style=\"text-align:center;\">\n");
+      out.write("            <tbody>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td> CORREO: </td>\n");
+      out.write("                    <td> <input type=\"text\" name=\"texto1\" value=\"\" size=\"20\" /> </td>\n");
+      out.write("                    <td></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td> PASSWORD: </td>\n");
+      out.write("                    <td>  <input type=\"password\" name=\"texto2\" value=\"\" size=\"20\" /> </td>\n");
+      out.write("                    <td></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td><input type=\"submit\" value=\"ADD\" name=\"boton1\" /></td>\n");
+      out.write("                    <td><input type=\"submit\" value=\"REMOVE\" name=\"boton2\" /></td>\n");
+      out.write("                    <td><input type=\"submit\" value=\"BUSCAR\" name=\"boton3\" /></td>\n");
+      out.write("                </tr>\n");
+      out.write("            </tbody>\n");
+      out.write("        </table>\n");
+      out.write("        </font>\n");
+      out.write("    </center>     \n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
--- cliente/web/admin.jsp
+++ cliente/web/admin.jsp
@@ -12,6 +12,34 @@
         <title>Administrador</title>
     </head>
     <body  background="BACK2.jpg">
-       
+    <center>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1>Administrador</h1>
+        <br></br>
+        <br></br>
+        <font color="white">
+        <table border="0" style="text-align:center;">
+            <tbody>
+                <tr>
+                    <td> CORREO: </td>
+                    <td> <input type="text" name="texto1" value="" size="20" /> </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td> PASSWORD: </td>
+                    <td>  <input type="password" name="texto2" value="" size="20" /> </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="ADD" name="boton1" /></td>
+                    <td><input type="submit" value="REMOVE" name="boton2" /></td>
+                    <td><input type="submit" value="BUSCAR" name="boton3" /></td>
+                </tr>
+            </tbody>
+        </table>
+        </font>
+    </center>     
     </body>
 </html>
